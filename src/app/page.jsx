8b58b363ce3e1be415b272b83d234ded9de4f443
francois lalay <francois.lalay@gmail.com>
2023-08-19 15:54:35 +0200

    add basic template of homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,8 +2,14 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Bonjour les gens</h1>
+    <main className="flex flex-col h-screen">
+      <header className='bg-slate-200'>je suis le header</header>
+      <div className='bg-slate-600 flex justify-between h-full '>
+        <nav className='bg-red-200'>je suis la sidebar a gauche</nav>
+        <section className='bg-green-200 basis-full'>je suis la section centrale</section>
+        <div className='bg-orange-200 '>je suis la sidebar a droite</div>
+      </div>
+      
     </main>
   )
 }
