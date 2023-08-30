730a0bbbfc30917850a5b1d8d8bed7862c2a9014
francois lalay <francois.lalay@gmail.com>
2023-08-30 22:19:18 +0200

    add style on navbar on left and create UserInitial

---
+++ src/components/global/UserInitial.jsx
@@ -0,0 +1,13 @@
+
+
+export default function UserInitial({user}) {
+  return (
+    <div className="bg-sky-500 rounded-full w-fit h-fit p-2 flex items-center justify-center text-md font-semibold  ">
+      {initial(user.firstname, user.lastname)}
+    </div>
+  )
+}
+
+const initial = (firstname, lastname) => {
+  return firstname.substring(0,1) + lastname.substring(0,1)
+}
--- src/components/sidebar/NavSidebar.jsx
+++ src/components/sidebar/NavSidebar.jsx
@@ -1,16 +1,29 @@
 import Link from "next/link.js";
 import SignOutButton from "../global/SignOutButton.jsx";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route.js";
+import UserInitial from "../global/UserInitial.jsx";
 
 export default async function NavSidebar(){
-  const projects = await getData("http://atd16-api.test/api/projects")
+  const session = await getServerSession(authOptions);
+
+  const user = await getData(
+    "http://atd16-api.test/api/me",
+    session.user.token
+  );
   
+
   return(
-    <nav className=" w-1/6 text-sm flex flex-col pt-10">
+    <nav className="text-sm flex flex-col px-1 w-1/5">
+      <div className="flex items-center gap-2">
+        <UserInitial user={user}/>
+        <h2 className="text-md font-semibold">{`${user.firstname} ${user.lastname}`}</h2>
+      </div>
       <SignOutButton>Se déconnecter</SignOutButton>
       <ul className="flex flex-col gap-4">
         <li><Link href={'/'}>Mes tâches</Link></li>
         <li><Link href={'/projets'}>Les projets</Link></li>
-        {projects['hydra:member'].map((project) => (
+        {user.projects.map((project) => (
           <li key={project.id}><Link href={`/projets/${project.id}`} >{project.name}</Link></li>
         ))}
       </ul>
@@ -19,9 +32,12 @@ export default async function NavSidebar(){
 }
 
 //fonction qui appel les données sur l'api
-async function getData(url) {
+async function getData(url, token) {
   const res = await fetch(url, {
     cache: "no-store",
+    headers: {
+      Authorization: `Bearer ${token} `,
+    },
   });
   if (!res.ok) {
     throw new error("Failed to fetch data");
