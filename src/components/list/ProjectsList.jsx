bc65801116e620cb39881d67658bff3034dfee6d
francois lalay <francois.lalay@gmail.com>
2023-09-01 10:15:48 +0200

    change style in projectlist and userlist

--- src/components/list/ProjectsList.jsx
+++ src/components/list/ProjectsList.jsx
@@ -1,11 +1,37 @@
 export default function ProjectsList({ projects, onSelect }) {
   return (
-    <ul>
+    <div className="p-10 border-t-2 mt-10">
+      <table className="table-fixed divide-y text-left w-full text-sm">
+      <thead>
+        <tr className="text-neutral-500">
+          <th className="py-4">Nom du projet</th>
+          <th>Status</th>
+          <th>Deadline</th>
+          <th>Date de création</th>
+          <th>Contributeurs</th>
+        </tr>
+      </thead>
+      <tbody className="divide-y ">
       {projects.map((project) => (
-        <li onClick={() => onSelect(project)} key={project.id}>
-          {project.name}
-        </li>
+        <tr onClick={() => onSelect(project)} key={project.id} className="hover:bg-neutral-200 cursor-pointer ">
+          <td className="truncate pr-2" >{project.name}</td>
+          <td className=" flex gap-2 items-center " ><div className="bg-green-500 h-3 w-3 rounded-full"></div><div className="py-4">{project.status}</div></td>
+          <td>{formatDateToDDMMYYYY(project.deadline)}</td>
+          <td>{formatDateToDDMMYYYY(project.createdAt)}</td>
+          <td >{`${project.contributor.length}`}</td>
+        </tr>
       ))}
-    </ul>
+      </tbody>
+    </table>
+    </div>
   );
+}
+
+function formatDateToDDMMYYYY(date) {
+  const newDate = new Date(date)
+  const year = newDate.getFullYear();
+  const month = String(newDate.getMonth() + 1).padStart(2, "0");
+  const day = String(newDate.getDate()).padStart(2, "0");
+
+  return `${day}-${month}-${year}`;
 }
--- src/components/list/UsersList.jsx
+++ src/components/list/UsersList.jsx
@@ -1,22 +1,28 @@
-export default function UsersList({users, onSelect}){
-  return(
-    <table className="table-fixed bg-green-100">
-      <thead>
-        <tr>
-          <th>Prénom</th>
-          <th>Nom</th>
-          <th>Email</th>
-        </tr>
-      </thead>
-      <tbody>
-        {users['hydra:member'].map((user)=>(
-          <tr key={user.id} onClick={()=> onSelect(user)}>
-            <td>{user.firstname}</td>
-            <td>{user.lastname}</td>
-            <td>{user.email}</td>
+export default function UsersList({ users, onSelect }) {
+  return (
+    <div className="p-10 border-t-2 mt-10">
+      <table className="table-fixed divide-y text-left w-full text-sm">
+        <thead>
+          <tr className="text-neutral-500">
+            <th className="py-4">Prénom</th>
+            <th>Nom</th>
+            <th>Email</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
-  )
+        </thead>
+        <tbody className="divide-y ">
+          {users["hydra:member"].map((user) => (
+            <tr
+              onClick={() => onSelect(user)}
+              key={user.id}
+              className="hover:bg-neutral-200 cursor-pointer "
+            >
+              <td className="truncate pr-2 py-4">{user.firstname}</td>
+              <td className="truncate pr-2">{user.lastname}</td>
+              <td className="truncate pr-2">{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
